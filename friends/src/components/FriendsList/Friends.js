7d9e93cc5e98e4c9583870fc46a1b53d2f181474
Zech <zechdrink@aol.com>
2019-02-27 14:49:05 -0800

    added the friends data to Apps state and created files for my components to be

---
+++ friends/src/components/FriendsList/Friends.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+const Friends = props => {
+
+return (
+    <div>
+        I am Friend.
+    </div>
+)
+
+}
+
+export default Friends;
---
+++ friends/src/components/FriendsList/FriendsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Friends from './Friends.js';
+
+
+const FriendsList = props => {
+
+    return (
+        <div>
+            I am FriendsList
+            <Friends />
+        </div>
+    )
+
+}
+
+export default FriendsList;
